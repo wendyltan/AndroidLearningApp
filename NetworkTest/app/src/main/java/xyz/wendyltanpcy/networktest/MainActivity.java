9855f0702d64215a87e743da029d2831f1a2abbf
Wendyltanpcy <youlinai233@gmail.com>
2017-09-13 22:11:02 +0800

    adding new Test

---
+++ NetworkTest/app/src/main/java/xyz/wendyltanpcy/networktest/MainActivity.java
@@ -0,0 +1,111 @@
+package xyz.wendyltanpcy.networktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.send_request){
+            sendRequestWithOkhttp();
+        }
+    }
+
+    private void sendRequestWithOkhttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://www.w3school.com.cn/example/xmle/note.xml")
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+//                    showResponse(responseData);
+                    parseXMLWithPull(responseData);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void parseXMLWithPull(String responseData) {
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(responseData));
+            int eventType = xmlPullParser.getEventType();
+            String to = "";
+            String from ="";
+            String heading = "";
+            String body = "";
+            while (eventType!=xmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    case XmlPullParser.START_TAG:{
+                        if("to".equals(nodeName)){
+                            to = xmlPullParser.nextText();
+                        }else if("from".equals(nodeName)){
+                            from = xmlPullParser.nextText();
+                        }else if("heading".equals(nodeName)){
+                            heading = xmlPullParser.nextText();
+                        }else if("body".equals(nodeName)){
+                            body = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG:{
+                        if ("note".equals(nodeName)){
+                            Log.d("MainActivity","to is "+ to);
+                            Log.d("MainActivity","from is "+ from);
+                            Log.d("MainActivity","heading is "+heading);
+                            Log.d("MainActivity","body is " +body);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void showResponse(final String responseData) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseText.setText(responseData);
+            }
+        });
+    }
+}
